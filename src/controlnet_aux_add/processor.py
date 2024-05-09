9d6ac83454789b9c4535af7a8e74bd3b3e2a436d
KUN <leeguandon@gmail.com>
2024-05-09 11:00:15 +0800

    Add files via upload

---
+++ src/controlnet_aux_add/processor.py
@@ -0,0 +1,92 @@
+"""
+This file contains a Processor that can be used to process images with controlnet aux processors
+"""
+import io
+import logging
+from typing import Dict, Optional, Union
+
+from PIL import Image
+
+from controlnet_aux_add import (DepthEstimator, ImageUperNetSegmentor)
+
+LOGGER = logging.getLogger(__name__)
+
+MODELS = {
+    'dpt': {"class": DepthEstimator, "checkpoint": False},
+    'upernet': {"class": ImageUperNetSegmentor, "checkpoint": False}
+}
+
+MODEL_PARAMS = {
+    'dpt': {},
+    'upernet': {}
+}
+
+CHOICES = f"Choices for the processor are {list(MODELS.keys())}"
+
+
+class Processor:
+    def __init__(self, processor_id: str, params: Optional[Dict] = None) -> None:
+        """Processor that can be used to process images with controlnet aux processors
+
+        Args:
+            processor_id (str): processor name, options are 'hed, midas, mlsd, openpose,
+                                pidinet, normalbae, lineart, lineart_coarse, lineart_anime,
+                                canny, content_shuffle, zoe, mediapipe_face
+            params (Optional[Dict]): parameters for the processor
+        """
+        LOGGER.info(f"Loading {processor_id}")
+
+        if processor_id not in MODELS:
+            raise ValueError(
+                f"{processor_id} is not a valid processor id. Please make sure to choose one of {', '.join(MODELS.keys())}")
+
+        self.processor_id = processor_id
+        self.processor = self.load_processor(self.processor_id)
+
+        # load default params
+        self.params = MODEL_PARAMS[self.processor_id]
+        # update with user params
+        if params:
+            self.params.update(params)
+
+    def load_processor(self, processor_id: str) -> 'Processor':
+        """Load controlnet aux processors
+
+        Args:
+            processor_id (str): processor name
+
+        Returns:
+            Processor: controlnet aux processor
+        """
+        processor = MODELS[processor_id]['class']
+
+        # check if the proecssor is a checkpoint model
+        if MODELS[processor_id]['checkpoint']:
+            processor = processor.from_pretrained("lllyasviel/Annotators")
+        else:
+            processor = processor()
+        return processor
+
+    def __call__(self, image: Union[Image.Image, bytes],
+                 to_pil: bool = True) -> Union[Image.Image, bytes]:
+        """processes an image with a controlnet aux processor
+
+        Args:
+            image (Union[Image.Image, bytes]): input image in bytes or PIL Image
+            to_pil (bool): whether to return bytes or PIL Image
+
+        Returns:
+            Union[Image.Image, bytes]: processed image in bytes or PIL Image
+        """
+        # check if bytes or PIL Image
+        if isinstance(image, bytes):
+            image = Image.open(io.BytesIO(image)).convert("RGB")
+
+        processed_image = self.processor(image, **self.params)
+
+        if to_pil:
+            return processed_image
+        else:
+            output_bytes = io.BytesIO()
+            processed_image.save(output_bytes, format='JPEG')
+            return output_bytes.getvalue()
